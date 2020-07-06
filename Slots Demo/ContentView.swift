4d6f7dd72b19022cbe98b692c0a7d8df7c67b7a2
Alex Sapsford <asapsford11@gmail.com>
2020-07-06 15:02:41 +0100

    Initial Commit

---
+++ Slots Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots Demo
+//
+//  Created by Alex Sapsford on 06/07/2020.
+//  Copyright © 2020 Alex Sapsford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
